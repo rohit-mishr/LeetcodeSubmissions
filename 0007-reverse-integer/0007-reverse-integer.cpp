bb7928ce756d034453b8443c9cf204f1994f8486
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2025-12-06 22:36:35 +0530

    Time: 6 ms (5.29%), Space: 8.4 MB (97.39%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -7,9 +7,10 @@ public:
         while(q!=0){
             rem = q%10;
             q/=10;
-            if(result > INT_MAX/10 || result < INT_MIN/10){
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && rem > 7))
                 return 0;
-            }
+            if (result < INT_MIN / 10 || (result == INT_MIN / 10 && rem < -8))
+                return 0;
             result = result*10 + rem;
         }
         return result;
