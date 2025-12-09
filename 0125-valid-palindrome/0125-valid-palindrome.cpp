6d335d895167db89d0b5c277f97f4a2726ea6c82
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2025-12-09 22:39:18 +0530

    Time: 0 ms (100%), Space: 9.8 MB (78.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int end = s.length()-1;
-        int start = 0;
-        while(start<end){
-            while(start<end){
-                if(!isalnum(s[start])){
-                    start++;
-                }
-                else{
-                    break;
-                }
-            }
-            while(start<end){
-                if(!isalnum(s[end])){
-                    end--;
-                }
-                else{
-                    break;
-                }
-            }
-            if(tolower(s[start])!=tolower(s[end])){
-                cout<<start<<" "<<end;
-                return false;
-            }
-            start++;
-            end--;
+        int right = s.length()-1;
+        int left = 0;
+        while (left < right) {
+        // Skip non-alphanumeric characters on the left side
+        if (!isalnum(s[left])) 
+            left++;
+        // Skip non-alphanumeric characters on the right side
+        else if (!isalnum(s[right])) 
+            right--;
+        // If characters are different, it's not a palindrome
+        else if (tolower(s[left]) != tolower(s[right])) 
+            return false;
+        else {
+            // Move towards the middle if characters are the same
+            left++; 
+            right--;
+        }
         }
         return true;
     }
