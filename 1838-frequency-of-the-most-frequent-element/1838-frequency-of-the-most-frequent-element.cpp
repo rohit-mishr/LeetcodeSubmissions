44effabf77ba85f77efba74f07bd721a5a7dbdd1
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2025-12-10 21:17:31 +0530

    Time: 33 ms (83.08%), Space: 105.7 MB (91.61%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-    int n = nums.size();
-    long long actual_sum = 0;         // use long long for sums
-    int start = 0;
-    int max_freq = 0;
-
-    for (int end = 0; end < n; ++end) {
-        actual_sum += nums[end];
-
-        // required ops to make nums[start..end] all equal to nums[end]
-        while ((long long)(end - start + 1) * nums[end] - actual_sum > k) {
-            if((end-start+1)==max_freq){
-                break;
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int max_freq = 0;
+        int start = 0;
+        long long actual_sum = 0;
+        for(int end = 0 ; end < n ; end++){
+            actual_sum += nums[end];
+            while((long long)(end - start + 1 )*nums[end] > (actual_sum + k)){
+                if((end-start+1) == max_freq){
+                    break;
+                }
+                actual_sum-=nums[start++];
             }
-            actual_sum -= nums[start++];
+            max_freq = max(max_freq,(end-start+1));
         }
-
-        max_freq = max(max_freq, end - start + 1);
-    }
-
-    return max_freq;
+        return max_freq;
     }
 };
