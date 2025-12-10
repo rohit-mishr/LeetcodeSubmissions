3e67e19eb63774b3ce24b546aed446be267f2f38
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2025-12-10 20:06:09 +0530

    Time: 36 ms (70.25%), Space: 105.7 MB (99.79%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -12,9 +12,9 @@ public:
 
         // required ops to make nums[start..end] all equal to nums[end]
         while ((long long)(end - start + 1) * nums[end] - actual_sum > k) {
-            // if((start-end+1)==max_freq){
-            //     continue;
-            // }
+            if((start-end+1)==max_freq){
+                continue;
+            }
             actual_sum -= nums[start++];
         }
 
