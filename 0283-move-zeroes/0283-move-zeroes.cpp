d322b0a84ae3f0cc1447cca084723095cdc08684
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2025-12-12 03:08:52 +0530

    Time: 0 ms (100%), Space: 23.6 MB (98.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,15 +3,12 @@ public:
     void moveZeroes(vector<int>& nums) {
         int slow = 0;
         int n = nums.size();
-        for(;slow<n;slow++){
-            if(nums[slow]==0){
-                break;
-            }
-        }
-        for(int fast = slow+1 ; fast< n ; fast++){
+        for(int fast = 0 ; fast < n ; fast++){
             if(nums[fast]!=0){
                 nums[slow] = nums[fast];
-                nums[fast] = 0;
+                if(fast!=slow){
+                    nums[fast] = 0;
+                }
                 slow++;
             }
         }
