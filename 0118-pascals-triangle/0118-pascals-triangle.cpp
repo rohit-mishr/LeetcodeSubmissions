4c561bea9d96e45648c1e1459fa78451e1561380
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2025-12-21 19:04:41 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.97%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,15 +3,11 @@ public:
     vector<vector<int>> generate(int r) {
         vector<vector<int>> ans;
         ans.reserve(r);
-        ans.push_back({1});
-        for(int i = 1 ; i < r ; i++){
-            vector<int> row;
-            row.reserve(i+1);
-            row.push_back(1);
+        for(int i = 0 ; i < r ; i++){
+            vector<int> row(i+1,1);
             for(int j = 1 ; j < i ; j++){
-                row.push_back(ans[i-1][j-1]+ans[i-1][j]);
+                row[j] = (ans[i-1][j-1]+ans[i-1][j]);
             }
-            row.push_back(1);
             ans.push_back(row);
         }
         return ans;
