d9397979534d0c949862ac8ee9c92095b7269e9a
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2025-12-22 17:53:22 +0530

    Time: 0 ms (100%), Space: 12.2 MB (95.52%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(m==0){
-            nums1 = nums2;
-            return ;
-        }
         if(n==0){
             return;
         }
