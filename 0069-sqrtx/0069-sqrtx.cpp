5a57c582f0b65ce51abaf1e7ab1bcf709f978752
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2026-01-03 18:27:51 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.4%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-       return floor(sqrt(x));
+        int low = 1;
+        int high = x;
+        int ans = 0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if((long long)mid*mid <= x){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
     }
 };
