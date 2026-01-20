1e7769561a9e679291fbe7a438955088067bd049
Rohit Mishra <130590624+rohit-mishr@users.noreply.github.com>
2026-01-20 18:00:35 +0530

    Time: 0 ms (100%), Space: 10.7 MB (90.38%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        pair<int,char> freq[256];
+        string ans;
+        for(int i = 0 ; i < 256 ; i++){
+            freq[i] = {0,i};
+        }
+        for(auto c : s){
+            freq[c].first++;
+        }
+        sort(freq,freq + 256,[](pair<int,char> f , pair<int,char> sec){
+            if(f.first>sec.first) return true;
+            if(f.first<sec.first) return false;
+            return f.second > sec.second;
+        });
+        for(auto i : freq){
+            if(i.first!=0){
+                int temp = i.first;
+                while(temp--){
+                    ans+=i.second;
+                }
+            }
+        }
+        return ans;
+    }
+};
